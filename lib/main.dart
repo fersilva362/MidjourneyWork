ea1d22482e2ac4c51be4a2edb05f081b518163fe
fersilva362 <fersilva362@gmail.com>
2024-02-03 10:59:17 -0300

    env

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_application_1/features/create_prompt_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: "env");
   runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
